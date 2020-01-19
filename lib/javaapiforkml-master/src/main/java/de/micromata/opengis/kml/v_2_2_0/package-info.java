05fb6563de99b16f616507ee1ca86f04ffe21573
YaliToledano <47944075+YaliToledano@users.noreply.github.com>
2020-01-19 21:31:13 +0200

    KML_Logger

--- lib/javaapiforkml-master/src/main/java/de/micromata/opengis/kml/v_2_2_0/package-info.java
+++
@@ -1,5 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.opengis.net/kml/2.2", xmlns = {
-    @javax.xml.bind.annotation.XmlNs( prefix = "kml", namespaceURI = "http://www.opengis.net/kml/2.2" )
-},
-elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package de.micromata.opengis.kml.v_2_2_0;
